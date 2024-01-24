f048a76d3c088a6150be4ff19b0eeecb991f4dd1
ao420 <ao420@ic.ac.uk>
2024-01-24 15:46:03 +0000

    New, complete version to integrate

---
+++ RL_to_integrate_24Jan.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+class Node:
+    def __init__(self, buffer_size, service_time):
+        self._buffer_size = buffer_size
+        self._service_time = service_time
+        self.queue_length = 0
+
+def epsilon_greedy(state, Q, epsilon):
+    if np.random.rand() < epsilon:
+        return np.random.choice(len(Q[state]))
+    else:
+        return np.argmax(Q[state])
+
+def dyna_q(n_episodes, alpha, gamma, epsilon_i, n, nodes, A):
+    """
+    Function which trains the Dyna-Q learning agent.
+    -   state = node that agent is at
+    -   action = routing proportion at this node (action space is different for
+        every node)
+    -   reward = service_time*queue_length, summed over all nodes and *(-1)
+    -   Q and A are DICTIONARIES rather than tables because action space is
+        different for every node
+    
+    State and actions are identified by their indices. States: keys in the
+    dictionaries Q and A. Actions: indices in the numpy array Q[key] or A[key]
+
+    Args:
+        n_episodes (int)    : number of epsiodes to train agent on
+        alpha (float)       : learning rate (alpha)
+        gamma (float)       : discount factor
+        epsilon_i (float)   : initial value of epsilon
+        n (int)             : number of times planning loop is executed
+        nodes (list)        : list of node instances
+        A (dict)            : key = node index, value = np.ndarray of actions
+    """
+    # Initialise Q(s,a) and Model(s,a) for all states and actions
+    keys = np.arange(len(nodes))            # indices from 0, ... n_nodes-1
+    Q = dict.fromkeys(keys, 0)
+    for k in A.keys():
+        Q[k] = np.zeros(A[k].shape)
+    Model = Q.copy()
+    
+    epsilon = epsilon_i
+    for _ in range(n_episodes):                                     # loop forever
+        # visited = {key = state :value = set of taken actions}
+        visited = {}
+        state = np.random.choice(len(nodes))                        # (a) 
+        action = epsilon_greedy(state, Q, epsilon)                  # (b)
+
+        if state in visited.keys():
+            visited[state].add(action)
+        else:
+            visited[state] = set([action])
+
+        next_state, reward = get_next_state(state, action)          # (c)
+
+        # (d)
+        Q[state][action] += alpha*(reward + \
+                                gamma*np.max(Q[next_state]) - \
+                                    Q[state][action])
+        
+        Model[state][action] = (reward, next_state)                 # (e)
+
+        for _ in range(n):
+            s = np.random.choice(list(visited.keys()))
+            a = np.random.choice(list(visited[s]))
+            reward, next_state = Model[s][a]
+            Q[s][a] += alpha*(reward + \
+                                gamma*np.max(Q[next_state]) - \
+                                Q[s][a])
+        
+        epsilon *= 0.999
+
+
+
+if __name__ == "__main__":
+    pass
