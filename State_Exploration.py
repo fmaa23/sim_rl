5d513220ad4907df158b3bcb49eb3726c08b3efa
jw923 <jinyan.wang23@imperial.ac.uk>
2024-02-07 16:19:03 +0000

    update state exploration

--- State_Exploration.py
+++ State_Exploration.py
@@ -1,14 +1,18 @@
 from tensorflow import random
-from Aaron_Part import DDPGAgent
 import numpy as np
 import itertools
 
-def explore_state(w1 = 0.5, w2 = 0.5, epsilon = 1):
+def explore_state(DDPG_agent, queue_model, num_sample, visit_counts, w1 = 0.5, w2 = 0.5, epsilon = 1):
     """
     This function is used for state exploration for real word environment
     It helps the agent decide which state to explore next based on factors, including Q-values and visit counts. 
     
     Parameters:
+    - DDPG_agent: the class of the DDPGAGent
+    - queue_model: the class of the queue model
+    - num_sample: number of states we sample from to get the lowest reward and times visited
+    - visit_counts: a dictionary that stores the number of times being visited for each state;
+    The key is the tuple of the state and the value is the number of time visited
     - w1: Weight for the influence of Q-values (default: 0.5)
     - w2: Weight for the influence of visit counts (default: 0.5)
     - epsilon: Probability of exploration (default: 1)
@@ -17,9 +21,21 @@ def explore_state(w1 = 0.5, w2 = 0.5, epsilon = 1):
     - chosen_state: The selected state for exploration.
     """
 
+    max_buffer_size = []
+    for q_buffer in queue_model.q_args.keys():
+        max_buffer = queue_model.q_args[q_buffer]
+        max_buffer_size.append(max_buffer)
+    
+    sample_states = []
+    for _ in range(num_sample):
+        array = np.array([np.random.randint(0, max_val) for max_val in max_buffer_size])
+        sample_states.append(array)
+
+    states_array = np.array(sample_states)
+
     # explore state with lower Q values and being visited less frequently
-    states_ordered_by_reward = rank_states_by_Q_values()
-    states_ordered_by_visits = rank_states_by_visits()
+    states_ordered_by_reward = rank_states_by_Q_values(DDPG_agent)
+    states_ordered_by_visits = rank_states_by_visits(visit_counts, states_array)
 
     reward_rankings = {state[0]: rank for rank, state in enumerate(states_ordered_by_reward)}
     visit_rankings = {state[0]: rank for rank, state in enumerate(states_ordered_by_visits)}
@@ -44,7 +60,7 @@ def explore_state(w1 = 0.5, w2 = 0.5, epsilon = 1):
 
     return chosen_state
 
-def rank_states_by_Q_values(self):
+def rank_states_by_Q_values(DDPG_agent, states_array):
     """
     This function ranks states based on their Q-values. 
     The function first generates all possible states based on the maximum buffer sizes.
@@ -57,46 +73,16 @@ def rank_states_by_Q_values(self):
     Returns:
     - sorted_state_q_value_pairs: A list of state-Q-value pairs sorted in ascending order of Q-values.
     """
+    
+    q_values = DDPG_agent.critic_network.forward(states_array)
 
-    max_buffer_size = []
-    for q_buffer in self.queue_model.q_args.keys():
-        max_buffer = self.queue_model.q_args[q_buffer]
-        max_buffer_size.append(max_buffer)
-    
-    queue_ranges = [range(max_buffer_size[i]) for i in range(len(max_buffer))]
-    all_states = np.array(list(itertools.product(*queue_ranges)))
-    
-    all_q_values = []
-    batch_size = 64
-    for i in range(0, len(all_states), batch_size):
-        batch_states = all_states[i:i+batch_size]
-        batch_q_values = self.critic_network.forward(batch_states)
-        all_q_values.append(batch_q_values)
-    
-    max_q_value = np.max(all_q_values)  
-    normalized_q_values = all_q_values / max_q_value  
-
-    state_q_value_pairs = list(zip(all_states, normalized_q_values))
+    state_q_value_pairs = list(zip(states_array, q_values))
     sorted_state_q_value_pairs = sorted(state_q_value_pairs, key=lambda x: x[1])
 
     return sorted_state_q_value_pairs
 
-def track_state(visit_counts, state_indices):
-    """
-    This function tracks the number of times each state is being visited.
 
-    Parameters:
-    - visit_counts: a numpy array that tracks the number of times visited for each state 
-                    and has shape np.zeros(self.max_size_per_dimension, dtype=int)
-    - state_indicies: a tuple that represents the queue length for all nodes 
-
-    Returns: increment the number of times being visited for the provided state
-    """
-    
-    # Increment the visit count for a given state
-    visit_counts[state_indices] += 1
-
-def rank_states_by_visits(self):
+def rank_states_by_visits(visit_counts, states_array):
     """
     This function ranks states based on the number of visits they have received. 
     The function takes a matrix of visit counts and sorts the states in descending order of visit counts.
@@ -105,31 +91,19 @@ def rank_states_by_visits(self):
     - visit_counts: A matrix containing visit counts for each state.
 
     Returns:
-    - visited_states_and_counts: A list of visited states and their corresponding visit counts, sorted in descending order of visit counts.
+    - ordered_states_visit_dict: A dict of visited states and their corresponding visit counts, sorted in descending order of visit counts.
     """
-
-    flattened_visit_counts = self.visit_counts.flatten()
-    sorted_indices = np.argsort(flattened_visit_counts)[::-1]
-    sorted_visit_counts = flattened_visit_counts[sorted_indices]
-    multi_dim_indices = np.unravel_index(sorted_indices, self.visit_counts.shape)
-    ranked_states_and_counts = list(zip(zip(*multi_dim_indices), sorted_visit_counts))
-    visited_states_and_counts = [item for item in ranked_states_and_counts if item[1] > 0]
-    
-    return visited_states_and_counts
+    states_visit_dict = {}
+    for state in states_array:
+        if state in visit_counts:
+            num_visited = visit_counts[state]
+        else:
+            num_visited = 0
+    states_visit_dict[state] = num_visited
+    ordered_states_visit_dict = dict(sorted(states_visit_dict.items(), key=lambda item: item[1], reverse=True))
+    return ordered_states_visit_dict
 
     
 if __name__ == "__main__":
-    DDPG_agent = DDPGAgent()
-    num_stored_experiences = 0
-    batch_size = 100
-    
-    while True:
-        current_state = DDPG_agent.explore_state()
-        action = DDPG_agent.get_action(current_state)
-        #TODO: need to modify Queue_model.get_next_state() such that it returns reward, next_state, done
-        reward, next_state, done = DDPG_agent.model.get_next_state(action)
-        DDPG_agent.store_to_buffer(current_state, action, reward, next_state)
-        num_stored_experiences += 1
 
-        if num_stored_experiences > batch_size:
-            DDPG_agent.train()
+    explore_state(DDPG_agent, queue_model, num_sample, visit_counts, w1 = 0.5, w2 = 0.5, epsilon = 1)
