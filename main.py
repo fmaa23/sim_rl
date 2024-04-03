942c20b5c763e92ce02af15a4976ef5f88669add
Jevon-C <jevon.c.official@gmail.com>
2024-04-03 17:57:59 +0100

    Updating the yaml file for automatic testing

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from supporting_files.supporting_functions import start_train
+import sys
+from pathlib import Path
+# Get the absolute path of the parent directory (i.e., the root of your project)
+root_dir = Path(__file__).resolve().parent.parent
+# Add the parent directory to sys.path
+sys.path.append(str(root_dir))
+
+from Supporting_files.supporting_functions import start_train
 
 config_param_filepath = 'user_config/configuration.yml'
 eval_param_filepath = 'user_config/eval_hyperparams.yml'
