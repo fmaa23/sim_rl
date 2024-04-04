20971c7079b6017202ae16fd3e008e3897c83b98
jw923 <jinyan.wang23@imperial.ac.uk>
2024-04-04 09:46:00 +0800

    add docstrings to main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import sys
-from pathlib import Path
-# Get the absolute path of the parent directory (i.e., the root of your project)
-root_dir = Path(__file__).resolve().parent.parent
-# Add the parent directory to sys.path
-sys.path.append(str(root_dir))
 
-from Supporting_files.supporting_functions import start_train
+def add_path_to_system():
+    """
+    Adds the root directory of the current script to the system path.
+
+    This is particularly useful in situations where the script needs to import
+    modules from parent directories not originally in the Python system path,
+    facilitating a more flexible project structure without relying on the user
+    to modify their environment variables or system path manually.
+    """
+    import sys
+    from pathlib import Path
+    root_dir = Path(__file__).resolve().parent.parent
+    sys.path.append(str(root_dir))
 
 config_param_filepath = 'user_config/configuration.yml'
 eval_param_filepath = 'user_config/eval_hyperparams.yml'
@@ -14,4 +20,7 @@ data_filename = 'data'
 image_filename = 'images' 
 
 if __name__ == "__main__":
+    add_path_to_system()
+    from Supporting_files.supporting_functions import start_train
+
     start_train(config_param_filepath, eval_param_filepath, data_filename, image_filename)
